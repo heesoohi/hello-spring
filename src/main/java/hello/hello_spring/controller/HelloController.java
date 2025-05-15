dc5306e4cc666c28e571ebae7575b4bb1ca93d10
이희수 <leehsdd33@gmail.com>
2025-05-15 15:33:56 +0900

    feat: Hello 객체를 반환하는 /hello-api JSON 엔드포인트 추가

--- src/main/java/hello/hello_spring/controller/HelloController.java
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -1,8 +1,10 @@
 package hello.hello_spring.controller;
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -25,5 +27,25 @@ public class HelloController {
     @ResponseBody
     public String helloString(@RequestParam("name") String name) {
         return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
