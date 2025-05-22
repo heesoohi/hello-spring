d9be4e7ac1f35d38d88c5ed007d028920cab8594
이희수 <leehsdd33@gmail.com>
2025-05-23 07:13:36 +0900

    test: 테스트 간 상태 공유 문제 해결을 위해 afterEach에서 store 초기화

--- src/test/java/hello/hello_spring/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/hello/hello_spring/repository/MemoryMemberRepositoryTest.java
@@ -1,6 +1,7 @@
 package hello.hello_spring.repository;
 
 import hello.hello_spring.domain.Member;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -9,7 +10,12 @@ import static org.assertj.core.api.Assertions.*;
 
 class MemoryMemberRepositoryTest {
 
-    MemberRepository repository = new MemoryMemberRepository();
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @AfterEach
+    public void afterEach() {
+        repository.clearStore();
+    }
 
     @Test
     public void save() {
