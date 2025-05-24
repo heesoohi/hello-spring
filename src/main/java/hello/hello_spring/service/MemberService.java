8d1393d4a0efbbf2dc8767e816031fc0eaa7b8d0
이희수 <leehsdd33@gmail.com>
2025-05-24 15:47:54 +0900

    feat: 회원 가입 로직 추가

---
+++ src/main/java/hello/hello_spring/service/MemberService.java
@@ -0,0 +1,21 @@
+package hello.hello_spring.service;
+
+import hello.hello_spring.domain.Member;
+import hello.hello_spring.repository.MemberRepository;
+import hello.hello_spring.repository.MemoryMemberRepository;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    // 회원가입
+    public long join(Member member) {
+        memberRepository.findByName(member.getName())
+                        .ifPresent(m -> {
+                            throw new IllegalStateException("이미 존재하는 회원입니다.");
+                        });
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+}
