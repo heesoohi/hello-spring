2cdea0226dc468c88129ee147c502cd4ca5d7031
이희수 <leehsdd33@gmail.com>
2025-05-28 09:58:09 +0900

    test: 회원가입 로직 테스트코드 작성

---
+++ src/test/java/hello/hello_spring/service/MemberServiceTest.java
@@ -0,0 +1,37 @@
+package hello.hello_spring.service;
+
+import hello.hello_spring.domain.Member;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
+
+class MemberServiceTest {
+
+    MemberService memberService = new MemberService();
+
+    @Test
+    void 회원가입() {
+        // given
+        Member member = new Member();
+        member.setName("hello");
+
+        // when
+        long savedId = memberService.join(member);
+
+        // then
+        Member findMember = memberService.findOne(savedId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
