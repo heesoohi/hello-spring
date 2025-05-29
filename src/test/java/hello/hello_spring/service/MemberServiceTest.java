544cad3c0699bcf0603f406b2593c7e102e448c0
이희수 <leehsdd33@gmail.com>
2025-05-29 14:37:40 +0900

    test: 중복 회원 가입 시도 시 예외 처리 테스트 코드 작성

--- src/test/java/hello/hello_spring/service/MemberServiceTest.java
+++ src/test/java/hello/hello_spring/service/MemberServiceTest.java
@@ -28,6 +28,25 @@ class MemberServiceTest {
     }
 
     @Test
+    void 중복_회원_예외() {
+        // given
+        Member member1 = new Member();
+        member1.setName("spring");
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        // when
+        memberService.join(member1);
+        try {
+            memberService.join(member2);
+        } catch (IllegalStateException e) {
+            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+        }
+
+        // then
+    }
+
+    @Test
     void findMembers() {
     }
 
