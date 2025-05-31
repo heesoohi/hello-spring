473f707403a7889ca3eaa2481ad4aa614313bd19
이희수 <leehsdd33@gmail.com>
2025-05-31 11:11:11 +0900

    test: assertThrows 반환값 활용해 예외 메시지 검증 추가

--- src/test/java/hello/hello_spring/service/MemberServiceTest.java
+++ src/test/java/hello/hello_spring/service/MemberServiceTest.java
@@ -37,7 +37,9 @@ class MemberServiceTest {
 
         // when
         memberService.join(member1);
-        assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
 
 //        memberService.join(member1);
 //        try {
