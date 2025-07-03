816d39e4c7b5838025598a1ad097352e70231fbe
이희수 <leehsdd33@gmail.com>
2025-07-03 15:25:29 +0900

    refactor: TimeTraceAop를 스프링 빈으로 직접 등록

--- src/main/java/hello/hello_spring/aop/TimeTraceAop.java
+++ src/main/java/hello/hello_spring/aop/TimeTraceAop.java
@@ -5,7 +5,6 @@ import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
 @Aspect
-@Component
 public class TimeTraceAop {
 
     public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
