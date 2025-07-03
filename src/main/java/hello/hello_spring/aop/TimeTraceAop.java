cd77dc43d3ab0c58318c652904636fc9c2b4bdd5
이희수 <leehsdd33@gmail.com>
2025-07-03 15:29:20 +0900

    feat: TimeTraceAop 클래스에 @Around 어노테이션 적용

--- src/main/java/hello/hello_spring/aop/TimeTraceAop.java
+++ src/main/java/hello/hello_spring/aop/TimeTraceAop.java
@@ -1,12 +1,14 @@
 package hello.hello_spring.aop;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
 @Aspect
 public class TimeTraceAop {
 
+    @Around("execution(* hello.hello_spring..*(..))")
     public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
         long start = System.currentTimeMillis();
         System.out.println("START: " + joinPoint.toString());
